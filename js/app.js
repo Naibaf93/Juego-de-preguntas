6b28e412bcd08dd0da20232e26e243a2fb1b94ed
Fabián Huertas <miloh9318@hotmail.com>
2025-01-24 15:39:56 -0500

    funcion cuenta regresiva

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@ let tiempoTerminado;
 let intervaloTiempo;
 
 /* Funciones */
+function comenzarCuentaRegresiva(){
+    tiempoTerminado = setTimeout(tiempoCumplido, 3000);
+    intervaloTiempo = setInterval(ticTac, 1000);
+
+    document.getElementById("cuentaRegrevisa").textContent = 30;
+}
