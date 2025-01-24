9890e5333aa3e9264ad5d5e419417e3a0db7de8a
Fabián Huertas <miloh9318@hotmail.com>
2025-01-24 15:52:59 -0500

    funcion finalizado

--- js/app.js
+++ js/app.js
@@ -24,3 +24,21 @@ function tiempoCumplido() {
 
     alert("Game Over: se acabo el tiempo... intenta nuevamente");
 }
+
+function finalizado() {
+    clearTimeout(tiempoTerminado);
+    clearInterval(intervaloTiempo);
+
+    let fecha = new Date();
+    let respuesta1 = document.getElementById("respuesta1").value;
+    let respuesta2 = document.getElementById("respuesta2").value;
+    let respuesta3 = document.getElementById("respuesta3").value;
+    let respuesta4 = document.getElementById("respuesta4").value;
+    let respuesta5 = document.getElementById("respuesta5").value;
+
+    let mensaje = fecha.toLocaleDateString("es-ES") + "\n" + "1. " + respuesta1 + "\n" +        
+                                                                   + respuesta2 + "\n" +      
+                                                                   + respuesta3 + "\n" +
+                                                                   + respuesta4 + "\n" +
+                                                                   + respuesta5 + "\n" ;
+}
